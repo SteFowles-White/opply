2f6c7a8ddb5cb66b365477bdf77ebceb8b2dcd78
Stephen Fowles-White <FowlesWhite@gmail.com>
2023-10-21 09:22:50 +0100

    Add 404 page to site

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import SignIn from "../views/sign-in.vue";
 import Content from "../views/content.vue";
 import Suppliers from "../views/content/suppliers.vue";
 import Quotes from "../views/content/quotes.vue";
+import NotFound from "../views/not-found.vue";
 
 export default [
 	{
@@ -17,5 +18,11 @@ export default [
 			{ path: "/suppliers/:page", component: Suppliers }, // default tab
 			{ path: "/suppliers/quotes/:page", component: Quotes },
 		],
+	},
+
+	{
+		path: "/:pathMatch(.*)*",
+		name: "404",
+		component: NotFound,
 	},
 ];
---
+++ src/views/not-found.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="error">
+		<h1>Something went wrong, page can not be found</h1>
+		<h2>404 error</h2>
+
+		<p>Please click the button below to get back to the main site</p>
+		<router-link class="tab" :to="`/suppliers/1`">Return to site</router-link>
+	</div>
+</template>
+
+<style scoped>
+.error {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-content: center;
+	width: 100%;
+	height: 100vh;
+	background-color: var(--colour-light-blue);
+	text-align: center;
+}
+
+.error h1 {
+	font-size: 3rem;
+}
+
+.error h2 {
+	font-size: 1.5rem;
+	margin-bottom: 3rem;
+}
+
+.error p {
+	font-size: 1.5rem;
+	margin-bottom: 3rem;
+}
+
+.error a {
+	font-size: 1.5rem;
+	margin-bottom: 3rem;
+	color: var(--colour-black);
+}
+
+.error a:hover {
+	color: var(--colour-purple);
+	font-weight: 600;
+}
+</style>
